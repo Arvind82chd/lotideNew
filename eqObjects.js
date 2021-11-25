615687c5a9a56178d36e9ab22c294bd1ebb476d2
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-25 08:06:29 +0000

    added feature to test nested arrays in object using eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -1,27 +1,48 @@
 const eqObjects = function(object1, object2) {
-    let objLength1 = Object.keys(object1).length;
-    let objLength2 = Object.keys(object2).length;
-    if (objLength1 !== objLength2) {
-        return false;
-    } else {
-        for (let key in object1) {
-            if (object1[key] !== object2[key]) {
-                return false;
-            }
+  let objLength1 = Object.keys(object1).length;
+  let objLength2 = Object.keys(object2).length;
+  //console.log(Object.values(object1));
+  //console.log(Object.values(object2));
+  if (objLength1 !== objLength2) {
+    return false;
+  } else {
+     
+    for (let key in object1) {
+      const value1 = object1[key];
+      const value2 = object2[key];
+      if (Array.isArray(value1)
+        || Array.isArray(value2)) {
+        if (!eqArrays(value1, value2)) {
+          return false;
         }
+      } else {
+        if (value1 !== value2) {
+          return false;
+        }
+      }
     }
-    
+  }
+  return true;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
     return true;
-}
-
-
+  }
+};
 
 const assertEqual = function(actual, expected) {
-if (actual === expected) {
+  if (actual === expected) {
     console.log(` ✅✅✅  Assertion Passes: ${actual} === ${expected}`);
-} else {
+  } else {
     console.log(` 🛑🛑🛑  Assertion Fails: ${actual} !== ${expected}`);
-}
+  }
 };
 
 // Test Cases:
