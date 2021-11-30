034bd8d5c4bcf242678a117cc81b27a073ece839
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-30 02:10:54 +0000

    Refactored the letterPositions and its tests using mocsing mocha and chai

--- letterPositions.js
+++ letterPositions.js
@@ -12,32 +12,5 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-//console.log(letterPositions("hello").e, [1]);
-
-const assertArrayEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(` 🛑🛑🛑  Assertion Fails: ${array1} !== ${array2}`);
-  } else {
-    console.log(` ✅✅✅  Assertion Passes: ${array1} === ${array2}`);
-  }
-};
-  
-  
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-    return true;
-  }
-};
-
-// Test Cases:
-assertArrayEqual(letterPositions("hello").e, [1]);
-assertArrayEqual(letterPositions("hello world").l, [2,3,9]);
-assertArrayEqual(letterPositions("lighthouse in the house").h, [3,5,15,17]);
 
 module.exports = letterPositions;
