1dfdc5d8427790164ba132eebf4ccc209933d2e3
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-30 03:27:01 +0000

    Refactored countLetters with mocha and chai

--- countLetters.js
+++ countLetters.js
@@ -11,43 +11,6 @@ const countLetters = function(sentence) {
   } return results;
 };
     
-//Test
-console.log(countLetters("lighthouse in the house"));
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(` ✅✅✅  Assertion Passes: ${actual} === ${expected}`);
-  } else {
-    console.log(` 🛑🛑🛑  Assertion Fails: ${actual} !== ${expected}`);
-  }
-};
-
-// Test Case:
-let object = {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-};
-const result1 = countLetters("lighthouse in the house");
-for (let res in result1) {
-  if (result1[res] === 2) {
-    assertEqual(result1[res], 2);
-  } else if (result1[res] === 1) {
-    assertEqual(result1[res], 1);
-  } else if (result1[res] === 3) {
-    assertEqual(result1[res], 3);
-  } else if (result1[res] === 4) {
-    assertEqual(result1[res], 4);
-  }
-}
-
-//assertEqual(countLetters("lighthouse in the house"), object);
 
 module.exports = countLetters;
